5265e943fcaa204bd3d6437a0056aa84080578bb
Meng-Ting Wu <marttiwu@gmail.com>
2025-04-19 17:55:02 -0700

    add requirements.txt

--- run.sh
+++ run.sh
@@ -8,4 +8,8 @@
 # python main.py --use-ft
 
 # Run with fine-tuned model
-python main.py --use-ft --few-shot
+# python main.py --use-ft --few-shot
+
+# python main.py --use-ft --use-self-fix
+
+python main.py --use-self-fix
